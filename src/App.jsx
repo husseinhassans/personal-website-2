f0320bd176f10e07931d525ddd9fafdd7b713427
Hussein Hassan <h.hassan@ucla.edu>
2024-04-03 07:38:28 -0700

    sidebar added. There is a cursor blinking on the expanded sidebar. Need to fix this bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { Navbar } from "./components/navbar/Navbar";
 const App = () => {
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
       </section>
-      <section>Parallax</section>
+      <section id="Experience">Parallax</section>
       <section>Services</section>
-      <section>Parallax</section>
+      <section id="Projects">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Interests">Parallax</section>
+      <section>Interests</section>
+      <section id="About">Parallax</section>
+      <section>About</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,44 @@
+import { useEffect, useRef } from "react";
 import "./navbar.scss";
+import { motion } from "framer-motion";
+import Typed from "typed.js";
+import Sidebar from "../sidebar/Sidebar";
 
 export const Navbar = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["&lt;Hussein Hassan /&gt;"],
+      startDelay: 300,
+      typeSpeed: 30,
+      showCursor: true,
+      cursorChar: "|",
+      cursorBlinkSpeed: 1000,
+      loop: false,
+    });
+
+    // Cleanup
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="navbar">
       {/*Sidebar*/}
+      <Sidebar />
       <div className="wrapper">
-        <span> &lt;Hussein Hassan /&gt; </span>
+        {/* <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          &lt;Hussein Hassan /&gt;{" "}
+        </motion.span> */}
+        <div>
+          <span ref={el}></span>
+        </div>
         <div className="social">
           <a href="#">
             <img src="/linkedin.png" alt="" />
