c7b9ed269b444094028e981a9a35802840bcfd42
Hussein Hassan <h.hassan@ucla.edu>
2024-04-03 07:47:08 -0700

    minute 46:45

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,5 +1,15 @@
+import { motion } from "framer-motion";
+
 const ToggleButton = ({ setOpen }) => {
-  return <button onClick={() => setOpen((prev) => !prev)}>Button</button>;
+  return (
+    <button onClick={() => setOpen((prev) => !prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
+      </svg>
+    </button>
+  );
 };
 
 export default ToggleButton;
