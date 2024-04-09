f7fcb0cc5ce6c6f7a06c6ad3259342da523bb700
Hussein Hassan <h.hassan@ucla.edu>
2024-04-09 05:31:05 -0700

    parralax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import "./parallax.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
+import Typed from "typed.js";
 
 const Parallax = ({ type }) => {
   const ref = useRef();
@@ -10,6 +11,25 @@ const Parallax = ({ type }) => {
   });
   const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
   const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const text = type === "experience" ? "<Experience />" : "<Projects/>";
+
+  //   const el = useRef(null);
+  //   useEffect(() => {
+  //     const typed = new Typed(el.current, {
+  //       strings: [text],
+  //       startDelay: 300,
+  //       typeSpeed: 30,
+  //       showCursor: true,
+  //       cursorChar: "|",
+  //       cursorBlinkSpeed: 1000,
+  //       loop: false,
+  //     });
+
+  //     // Cleanup
+  //     return () => {
+  //       typed.destroy();
+  //     };
+  //   }, []);
 
   return (
     <div
@@ -22,9 +42,8 @@ const Parallax = ({ type }) => {
             : "linear-gradient(180deg, #111132, #505064)",
       }}
     >
-      <motion.h1 style={{ y: yText }}>
-        {type === "experience" ? "Experience" : "Projects"}
-      </motion.h1>
+      <motion.h1 style={{ y: yText }}>{text}</motion.h1>
+      <motion.h1 style={{ y: yText }}></motion.h1>
       <motion.div className="mountains"></motion.div>
       <motion.div className="planets" style={{ y: yBg }}></motion.div>
       <motion.div className="stars" style={{ x: yBg }}></motion.div>
