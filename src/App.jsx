82c58979783477235ca28b6ff71e29b89b5e1aed
Hussein Hassan <h.hassan@ucla.edu>
2024-04-17 23:16:17 -0700

    parrallax mobile

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,19 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      {/* <section id="Experience">
+      <section id="Experience">
         <Parallax type="experience" />
-      </section> */}
-      {/* <Experience />
+      </section>
+      <Experience />
       <section id="Projects">
         <Parallax type="projects" />
-      </section> */}
+      </section>
       {/* <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section> */}
-      {/* <section id="Academics">
+      <section id="Academics">
         <Parallax />
-      </section> */}
+      </section>
       {/* <section>Interests</section> */}
       {/* <section id="About">Parallax</section> */}
       {/* <section>About</section> */}
